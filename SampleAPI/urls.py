0d10a0187133c2e99cb3cb47f653cf0bbb39a826
Suman Kumar <suman.k@skysoft.net.in>
2019-07-26 13:29:53 +0530

    Homepage added

--- SampleAPI/urls.py
+++ SampleAPI/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
-from . import viewsets
+from . import viewsets, views
 
 # router = routers.DefaultRouter()
 # router.register(r'', viewsets.UserViewSet)
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('me', viewsets.UserViewSet.as_view(), name='user_profile'),
     path('user/register', viewsets.UserViewSet.as_view(), name='register_user'),
 ]
--- SampleApp/settings.py
+++ SampleApp/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'rest_framework',
+    'SampleAPI'
 ]
 
 MIDDLEWARE = [
